a9791380d791b2db868e128c6ffc4f5cc13817d2
skyway777 <yaskyway@gmail.com>
2016-03-28 20:20:03 +0300

    Update slushfile.js
    Added execute in format slush bem:mod block__elem_mod

--- slushfile.js
+++ slushfile.js
@@ -45,11 +45,15 @@ gulp.task('default', function (done) {
 
 gulp.task('block',function(done){
     run_gulp('block');
-})
+});
 
 gulp.task('elem',function(done){
     run_gulp('elem');
-})
+});
+
+gulp.task('mod',function(done){
+    run_gulp('mod');
+});
 
 function run_gulp(param) {
   var defaults = (function () {
@@ -71,21 +75,62 @@ function run_gulp(param) {
     if (require('fs').existsSync(configFile)) {
         user = require('iniparser').parseSync(configFile).user;
     }
-
-    return {
+    
+    var result = {
         appName: workingDirName,
         userName: osUserName || format(user.name || ''),
         authorName: user.name || '',
         authorEmail: user.email || '',
         folder:'common.blocks',
-        blockName:gulp.args[0],
-        elemName:gulp.args[1]
+        elemSplitter:'__',
+        modSplitter:'_',
+        blockName:'',
+        modName:'',
+        modValue:'',
+        elemName:'',
+        elemModName:'',
+        elemModValue:''
     };
+          
+    if (gulp.args[0])
+        {
+            var elemBlock = gulp.args[0].split(result.elemSplitter);
+            result.blockName = elemBlock[0];
+            var blockMod = result.blockName.split(result.modSplitter);
+            if (blockMod.length > 1)
+                {
+                    result.blockName = blockMod[0];
+                    result.modName = blockMod[1];
+                }
+            if (blockMod.length > 2)
+                {
+                    result.modValue = blockMod[2];
+                }
+            
+            if (elemBlock.length >1)
+            {
+                
+                result.elemName = elemBlock[1];                
+                var elemMod = result.elemName.split(result.modSplitter);
+                result.elemName = elemMod[0];
+            
+            if (elemMod.length > 1)
+                {
+                    result.elemName = elemMod[0];
+                    result.elemModName = elemMod[1];
+                }
+            if (elemMod.length > 2)
+                {
+                    result.elemModValue = elemMod[2];
+                }
+            }
+        }
+    
+      
+    return result;
 })();
 
-  if (param == "block") 
-  {
-    var prompts = [
+var prompts = [
     {
         name:'folder',
         message:'Enter folder (common.blocks):',
@@ -97,6 +142,9 @@ function run_gulp(param) {
         default:defaults.blockName
       }
     ];
+    
+  if (param == "block") 
+  {
     //Ask
     var exec = function (answers) {
         gulp.src(__dirname + '/templates/block/*')
@@ -117,22 +165,11 @@ function run_gulp(param) {
     inquirer.prompt(prompts,exec);
   }
   else if (param == "elem"){
-    var prompts = [
-    {
-        name:'folder',
-        message:'Enter folder :',
-        default:defaults.folder
-    },
-    {
-        name:'blockName',
-        message:'Enter name of Block',
-        default:defaults.blockName
-    },
-    {
+    prompts.push({
         name:'elemName',
         message:'Enter name of Element',
         default:defaults.elemName
-    }];
+    });
 
     var exec = function (answers) {
         answers.elemSplitter = "__";
@@ -145,7 +182,7 @@ function run_gulp(param) {
           .pipe(gulp.dest('./'+defaults.folder+'/'+answers.blockName+'/'+answers.elemSplitter+answers.elemName));
     };
 
-    if (gulp.args[0] && gulp.args[1])
+    if (gulp.args[0])
     {
       exec(defaults);
       return;
@@ -153,5 +190,94 @@ function run_gulp(param) {
     //Ask
     inquirer.prompt(prompts,
       exec);
-  } 
+    }
+    else if (param == "mod"){
+        prompts.push({
+            name:'modName',
+            message:'Enter name of Block Modifier',
+            default:defaults.modName
+        });
+        
+        prompts.push({
+            name:'modValue',
+            message:'Enter value of Block Modifier',
+            default:defaults.modValue
+        });
+        
+        prompts.push({
+            name:'elemModName',
+            message:'Enter name of Elem Modifier',
+            default:defaults.elemModName
+        });
+        
+        prompts.push({
+            name:'elemModValue',
+            message:'Enter value of Elem Modifier',
+            default:defaults.elemModValue
+        });
+        
+        var exec = function (answers) {
+            var dirName='';
+            if (answers.modName && answers.elemModName){
+                dirName='/templates/modElemMod'
+            }
+            else if (answers.modName){
+                dirName='/templates/mod'
+            }
+            else if (answers.elemModName){
+                dirName='/templates/elemMod'
+            }
+            
+            var path = './'+defaults.folder + '/' + answers.blockName;
+            var filename = answers.blockName;
+            
+            if (answers.modName != '')
+            {
+                path += '/'+defaults.modSplitter+answers.modName;
+                filename +=defaults.modSplitter+answers.modName;
+            }
+            
+            if (answers.modValue != '')
+            {
+                path += '/'+defaults.modSplitter+answers.modValue;
+                filename +=defaults.modSplitter+answers.modValue;
+            }
+            
+            if (answers.elemName != '')
+                {
+                    path += '/'+defaults.elemSplitter+answers.elemName;
+                    filename +=defaults.elemSplitter+answers.elemName;
+                }
+            
+            if (answers.elemModName != '')
+            {
+                path += '/'+defaults.modSplitter+answers.elemModName;
+                filename +=defaults.modSplitter+answers.elemModName;
+            }
+            
+            if (answers.elemModValue)
+            {
+                path += '/'+defaults.modSplitter+answers.elemModValue;
+                filename +=defaults.modSplitter+answers.elemModValue;
+            }
+            
+            
+            gulp.src(__dirname + dirName+'/*')
+              .pipe((function(){return template(answers);})())
+              .pipe(rename(function (file) {
+                file.basename = filename;
+              }))
+              .pipe(conflict(path))
+              .pipe(gulp.dest(path));
+        };
+
+        if (gulp.args[0])
+        {
+          exec(defaults);
+          return;
+        }
+        
+        inquirer.prompt(prompts,
+            exec);
+    }
 }
